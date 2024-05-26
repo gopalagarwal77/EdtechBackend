a1d0180e215bc675274d59740f8e6f85580d5fdf
Gopal Agarwal <93563296+gopalagarwal77@users.noreply.github.com>
2024-05-26 17:01:27 +0530

    Update razorpay.js

--- config/razorpay.js
+++ config/razorpay.js
@@ -1,6 +1,6 @@
 const Razorpay = require('razorpay');
 
 exports.instance = new Razorpay({
-    key_id: process.env.RAZORPAY_KEY || rzp_test_TzxQSmEJSA9Ca3,
+    key_id: process.env.RAZORPAY_KEY ,
     Key_secret: process.env.RAZORPAY_SECRET,
 }); 
